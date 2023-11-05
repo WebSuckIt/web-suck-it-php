617ab00f75681ad79f7a138932945878e466b762
wisdompraise <wisdompraise968@gmail.com>
2023-11-05 11:49:15 +0100

    Fixed example failing.

--- example/Base.php
+++ example/Base.php
@@ -1,24 +1,50 @@
 <?php
 require_once 'vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(getcwd());
+$dotenv->load();
+
 use Websuckit\WebsuckitPhp\Config;
 use Websuckit\WebsuckitPhp\Websuckit;
 use \Websuckit\WebsuckitPhp\Types\ChannelConnectionUrlConfig;
 
-$publicKey = "-----BEGIN RSA PUBLIC KEY-----
-MIIBCgKCAQEA0nvVnQiANN9iJ9g2DWby+XnRLsI4A0LTbbAT8VpHYs0in6p5cY8W
-MEX5qzzfp0/G1DaEwE1OTnObCraocovioFSiGlsBKdImXVyqig2j16HQDPsOOigP
-7vhJJgGJtDIPoTyxwPgEOlSgTWKHfZzRQGOiEhj0qVhQDc1wtgcnTb/XOn5HkI1t
-MLHHHOU4aSf3j0EhYdLAWD2XAEK2uGZbyPQVF3E+Yyh8Hh0ZoQ/DchGCmE6dlNOk
-PS6c3nxpjtg6uk1kAfZRqV0yhK+jx+Zmx0nam0f+raB4ARh/PvLczcx0gjzqXfY5
-s6lL3she6a8YoGfn/slNQlndoKrVD72zcQIDAQAB
------END RSA PUBLIC KEY-----
-";
+function generateRandomSlugWithHyphens($numWords = 2, $wordLength = 6) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyz';
+    $slug = '';
 
-$config = new Config("247dd2d6-5b30-45ad-8351-5c3c40eb0cd3", "tBZ3jHvdv4hzPNJ8QmYV", $publicKey);
+    for ($i = 0; $i < $numWords; $i++) {
+        $word = '';
+        for ($j = 0; $j < $wordLength; $j++) {
+            $randomIndex = rand(0, strlen($characters) - 1);
+            $word .= $characters[$randomIndex];
+        }
+        $slug .= $word;
+
+        if ($i < $numWords - 1) {
+            $slug .= '-';
+        }
+    }
+
+    return $slug;
+}
+
+$config = new Config($_ENV['USER_ID'], $_ENV['ACCESS_KEY'], $_ENV['PUBLIC_KEY']);
 $websuckit = new Websuckit($config);
-$channel = new ChannelConnectionUrlConfig("another", "pu6QFXFFp48ViVUIaEGg", true);
+$channel = new ChannelConnectionUrlConfig("another", $_ENV['CHANNEL_PASSKEY'], true);
 echo $websuckit->getConnectionUrl($channel);
 echo "\n";
 $res = $websuckit->getOrCreateChannel("another");
-echo json_decode($res, true)['pass_key'];
+
+echo json_encode($res);
+echo "\n";
+echo json_encode($websuckit->getChannel("another"));
+echo "\n";
+echo json_encode($websuckit->updateChannel($_ENV['TEST_CHANNEL_ID'], "another-new", false));
+echo "\n";
+$chan = $websuckit->createChannel(generateRandomSlugWithHyphens(2, 8));
+echo json_encode($chan);
+
+if(isset($chan['data']['channel']['id'])){
+    echo "\n";
+    echo json_encode($websuckit->deleteChannel($chan['data']['channel']['id']));
+}
