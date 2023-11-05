77d119be871794b9eae73ee2753d9b9f9a24f897
wisdompraise <wisdompraise968@gmail.com>
2023-11-05 12:45:09 +0100

    code refactoring.

--- example/Base.php
+++ example/Base.php
@@ -29,19 +29,13 @@ function generateRandomSlugWithHyphens($numWords = 2, $wordLength = 6) {
 
 $config = new Config($_ENV['USER_ID'], $_ENV['ACCESS_KEY'], $_ENV['PUBLIC_KEY']);
 $websuckit = new Websuckit($config);
-$channel = new ChannelConnectionUrlConfig("another", $_ENV['CHANNEL_PASSKEY'], true);
-echo $websuckit->getConnectionUrl($channel);
-echo "\n";
-$res = $websuckit->getOrCreateChannel("another");
-
-echo json_encode($res);
-echo "\n";
-echo json_encode($websuckit->getChannel("another"));
-echo "\n";
-echo json_encode($websuckit->updateChannel($_ENV['TEST_CHANNEL_ID'], "another-new", false));
-echo "\n";
+$channel = new ChannelConnectionUrlConfig($_ENV['CHANNEL_NAME'], $_ENV['CHANNEL_PASSKEY'], true);
+$websuckit->getConnectionUrl($channel);
+$res = $websuckit->getOrCreateChannel($_ENV['CHANNEL_NAME']);
+$websuckit->getChannel($_ENV['CHANNEL_NAME']);
+$websuckit->updateChannel($_ENV['TEST_CHANNEL_ID'], "another-new", false);
+$websuckit->updateChannel($_ENV['TEST_CHANNEL_ID'], $_ENV['CHANNEL_NAME'], false);
 $chan = $websuckit->createChannel(generateRandomSlugWithHyphens(2, 8));
-echo json_encode($chan);
 
 if(isset($chan['data']['channel']['id'])){
     echo "\n";
